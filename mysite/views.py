88a9a4e52861d597eb15e066d0f51b3ed614e430
combatdave <davidgundry@outlook.com>
2016-05-10 17:21:33 +0000

    db stuff

---
+++ mysite/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+from .models import Greeting
+
+
+def db(request):
+    greeting = Greeting()
+    greeting.save()
+
+    greetings = Greeting.objects.all()
+
+    return render(request, 'db.html', {'greetings': greetings})
